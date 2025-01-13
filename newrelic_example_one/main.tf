08d151d4878a1322e3e5a448fbcb0dacf3ddcd87
James Buchanan <jbuchanan@newrelic.com>
2025-01-13 10:42:16 +0000

    example patch

--- newrelic_example_one/main.tf
+++ newrelic_example_one/main.tf
@@ -1,4 +1,4 @@
 resource "newrelic_alert_policy" "policy" {
-  name                = "${var.name} / ${var.environment} ${local.tags.terraform} for (TerraformMonoRepo)"
+  name                = "${var.name} / ${var.environment} ${local.tags.terraform} for PATCH1  (TerraformMonoRepo)"
   incident_preference = "PER_POLICY"
 }
